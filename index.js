56d88cf33ff628a632c90c7cc63055a5ccdc670a
Mathew Hall <m@mhall.xyz>
2018-06-12 19:30:05 +0100

    Local html file displayed in the window

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const { app, BrowserWindow } = electron;
 
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({});
-    mainWindow.loadURL('https://mhall.xyz');
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
