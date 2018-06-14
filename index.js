a746775c2568511d776ad0b495492b11e538b417
Mathew Hall <m@mhall.xyz>
2018-06-14 20:33:57 +0100

    App working, reporting the seconds duration of a video

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
+let mainWindow;
+
 app.on('ready', () => {
-    const mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
-ipcMain.om('video:submit', path =>{
-
+ipcMain.on('video:submit', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+        mainWindow.webContents.send('video:metadata', metadata.format.duration);
+    });
 });
